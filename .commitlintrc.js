f9bee814def1ff107f2c84742b9d273489b43ea9
星宿君 <huxinfeng@github.com>
2022-07-13 17:39:53 +0800

    chore: 完善commit提交规范流程

---
+++ .commitlintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  /* type(scope?): subject  例子：feat(server): test */
+  extends: ['@commitlint/config-conventional'], // 使用预设的配置 https://github.com/conventional-changelog/commitlint/blob/master/@commitlint/config-conventional/index.js
+};
